ed30fc995dd033c0f3d4ad8ead8ad9c519456dc3
Yana07-Chaikovska <chaikovska_ak24@nuwm.edu.ua>
2026-05-02 16:05:04 +0300

    Add files via upload

---
+++ lab5/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const userName = "Яна";
+window.onload = () => {
+    document.body.innerHTML = "<h1>Привіт, Яна</h1>";
+};
+//# sourceMappingURL=index.js.map
---
+++ lab5/index.ts
@@ -0,0 +1,5 @@
+const userName: string = "Яна";
+
+window.onload = () => {
+    document.body.innerHTML = "<h1>Привіт, Яна</h1>";
+};
